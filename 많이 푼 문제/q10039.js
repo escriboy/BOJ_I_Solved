f783f0dd2f2d2325fcb560c07147d4f768bb51b4
escriboy <escriboy@gmail.com>
2022-09-15 16:50:04 +0900

    0915 문제풀이

--- 많이 푼 문제/q10039.js
+++ 많이 푼 문제/q10039.js
@@ -1,5 +1,19 @@
-const input = require('fs')
+let scores = require('fs')
   .readFileSync('dev/stdin')
   .toString()
+  .trim()
   .split('\n')
   .map((x) => Number(x));
+
+let total = 0;
+
+for (let i = 0; i < scores.length; i++) {
+  if(scores[i] < 40) {
+    scores[i] = 40;
+  }
+  total += scores[i];
+}
+
+const avg = total / 5;
+
+console.log(avg);
