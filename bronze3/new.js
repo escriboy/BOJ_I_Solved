40bcf4cca031a8e1774cd207832075bdb08e8c64
escriboy <escriboy@gmail.com>
2022-10-11 20:51:30 +0900

    1011 solved

---
+++ bronze3/new.js
@@ -0,0 +1,25 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+for (let i = 0; i < input.length; i++) {
+  const PI = Math.PI;
+  const N = input[i].split(' ').map(Number);
+  if (N[1] !== 0) {
+    const mile = (N[0] / 12 / 5280) * Math.PI;
+    const roll = N[1];
+    const second = N[2];
+    const distance = mile * roll; // mile
+    const avgSpeed = (distance / second) * 3600;
+    console.log(
+      `Trip #${i + 1}: ${distance.toFixed(2)} ${avgSpeed.toFixed(2)}`,
+    );
+  }
+}
+
+/*
+이동 거리 = 총 바퀴 회전 수 * 바퀴의 둘레(지름 * 파이)
+평균 속도 = 
+*/
