e267f583836b366874cdc58dfa32b63a159f0abb
YM CHOI <escriboy@gmail.com>
2022-12-27 05:55:40 +0900

    1227 solved

---
+++ bronze2/q5355.js
@@ -0,0 +1,26 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const cases = input.slice(1);
+
+for (const line of cases) {
+  const mars = line.split(' ');
+  let result = +mars[0];
+  for (let i = 1; i < mars.length; i++) {
+    if (mars[i] === '@') result *= 3;
+    if (mars[i] === '%') result += 5;
+    if (mars[i] === '#') result -= 7;
+  }
+  console.log(result.toFixed(2));
+}
+
+// const input = ['3', '3 @ %', '10.4 # % @', '8 #'];
+/**
+ * @ : x3
+ * % : +5
+ * # : -7
+ *
+ */
