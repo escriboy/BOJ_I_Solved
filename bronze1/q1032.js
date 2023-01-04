10355d99942cc86d86bf64ad8c2da5f540daa525
YM CHOI <escriboy@gmail.com>
2023-01-04 16:14:27 +0900

    0104 solved

---
+++ bronze1/q1032.js
@@ -0,0 +1,32 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+let N = +input[0];
+const name = [];
+
+for (let i = 1; i <= N; i++) {
+  name.push(input[i]);
+}
+
+solution(name);
+
+function solution(arr) {
+  let answer = [];
+  for (let i = 0; i < arr[0].length; i++) {
+    let tmp = arr[0][i];
+    let num = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (tmp !== arr[j][i]) {
+        answer.push('?');
+        break;
+      } else {
+        num++;
+      }
+      if (num === arr.length) answer.push(tmp);
+    }
+  }
+  console.log(answer.join(''));
+}
