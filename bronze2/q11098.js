314ac63479fa0edbd27eb7c740b6ced02419705b
YM CHOI <escriboy@gmail.com>
2023-01-09 14:50:04 +0900

    0109 solved

---
+++ bronze2/q11098.js
@@ -0,0 +1,35 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+let arr = input.slice(1);
+let yet = true;
+while (yet) {
+  let max = 0;
+  let buy_who = '';
+  for (let i = 1; i <= arr[0]; i++) {
+    let player = arr[i].split(' ');
+    let price = +player[0];
+    let name = player[1];
+    if (max < price) {
+      max = price;
+      buy_who = name;
+    }
+  }
+  console.log(buy_who);
+  arr = arr.slice(+arr[0] + 1);
+  if (arr.length === 0) yet = false;
+}
+
+// const input = [
+//   '2',
+//   '3',
+//   '10 Iversen',
+//   '1000000 Nannskog',
+//   '2000000 Ronaldinho',
+//   '2',
+//   '1000000 Maradona',
+//   '999999 Batistuta',
+// ];
