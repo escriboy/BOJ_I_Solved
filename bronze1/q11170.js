e372dd652c90a4fa647aefd37036c5a572310472
YM CHOI <escriboy@gmail.com>
2023-02-14 12:26:16 +0900

    0214 boj

---
+++ bronze1/q11170.js
@@ -0,0 +1,19 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const T = input.shift();
+
+for (const string of input) {
+  const [N, M] = string.split(' ').map(Number);
+  let count = 0;
+  for (let i = N; i <= M; i++) {
+    const arr = (i + '').split('');
+    for (let j = 0; j < arr.length; j++) {
+      arr[j] === '0' && count++;
+    }
+  }
+  console.log(count);
+}
